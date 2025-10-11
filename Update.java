d25231065e18c73299409750b1b42d587bf8979c
ASHISH MISHRA <84080229+ashishmishra3648@users.noreply.github.com>
2025-10-11 15:58:24 +0530

    Create Update.java

---
+++ Update.java
@@ -0,0 +1,43 @@
+import java.sql.*;
+public class Update {
+    public static void main(String[] args) throws ClassNotFoundException{
+        String url="jdbc:mysql://localhost:3306/mydatabase";
+        String username="root";
+        String password="10122001Am#";
+        String query=("update employee set job_title='Data Scientist', salary=100000.0 where id =3;");
+
+        //STEP 1: DRIVER LOADING
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded successfully!");
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            //STEP 2: CONNECTION ESTABLISHMENT
+            Connection con=DriverManager.getConnection(url, username, password);
+            // Connection is one Interface which is used to established connection btw
+            // Java program and Database.This Interface has DriverManager class.
+            //This class has one method getConnection which takes three parameters (url, username, password)
+            // con is instance of Connection interface.
+            System.out.println("Connection Established Successfully!");
+
+            //STEP 3: RUN SQL QUERY
+            Statement stm=con.createStatement();
+
+            int rows = stm.executeUpdate(query);
+            if(rows!=0){
+                System.out.println("Update Successful!");
+                System.out.println(rows+" row(s) affected.");
+            }else{
+                System.out.println("Update failed!");
+            }
+            stm.close();
+            con.close();
+            System.out.println("Connection Closed Successfully!");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
